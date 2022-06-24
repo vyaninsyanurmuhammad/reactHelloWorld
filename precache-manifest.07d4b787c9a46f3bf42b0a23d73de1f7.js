3b1cb367d74d9ae1f285a03abf4cfa386fccf063
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2022-06-24 23:17:01 +0700

    Updates

---
+++ precache-manifest.07d4b787c9a46f3bf42b0a23d73de1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc48321626ef8f8dd12424d445cc1e3",
+    "url": "/reactHelloWorld/index.html"
+  },
+  {
+    "revision": "fce1343bac5026354780",
+    "url": "/reactHelloWorld/static/css/main.21e89763.chunk.css"
+  },
+  {
+    "revision": "ac13dc7d96accc00ee8d",
+    "url": "/reactHelloWorld/static/js/2.69f4fe49.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/reactHelloWorld/static/js/2.69f4fe49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce1343bac5026354780",
+    "url": "/reactHelloWorld/static/js/main.fae3dfa8.chunk.js"
+  },
+  {
+    "revision": "08e82f2c5cbda30cc946",
+    "url": "/reactHelloWorld/static/js/runtime-main.533cde99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactHelloWorld/static/media/logo.5d5d9eef.svg"
+  }
+]);
